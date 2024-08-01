ae15474dc06f7a4acb18435ba0dce4f2d04d64c4
latif <latif@rhrk.uni-kl.de>
2024-08-01 15:35:21 +0200

    refactoring code

--- backend/scripts/retrieval_test.py
+++ backend/scripts/retrieval_test.py
@@ -1,8 +1,8 @@
 import asyncio
-from pymongo import MongoClient
 from llm import query_llm
 from evaluation.deep_eval import deep_evaluate
-import datetime
+
+TABLE_NAME = "RetrievalExperiment_with_papers"
 
 async def run_experiment(queries):
     # Loop through all queries
@@ -20,16 +20,14 @@ async def run_experiment(queries):
             
             # Evaluate and store results
             print("Evaluating response...")
-            metrics_scores = await deep_evaluate(query, response, retrieved_nodes,generated_queries,"RetrievalExperiment_with_papers")
+            metrics_scores = await deep_evaluate(query, response, retrieved_nodes,generated_queries,TABLE_NAME)
             print(f"Results for '{query}': {metrics_scores}")
             
         except Exception as e:
             print(f"Error occurred for query '{query}': {e}")
 
 queries = [
-"How can slime mold-inspired models help in understanding the distribution of dark matter?",
-"What biological principles can improve fault-tolerance in robotic systems inspired by ant trail maintenance?",
-"What natural mechanisms can inform the development of adaptive robotic grippers for delicate tasks?"
+"place all the queries here"
 ]
 
 
